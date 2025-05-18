e71038eaa5cbabb7cdb9c17bd8477c31d9bf8011
Charles Azam <azamcharles0@gmail.com>
2025-05-18 22:51:18 +0200

    fix: improve code for AI

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -5,5 +5,5 @@ def test_heatpump():
     from pyforge.examples.heat_pump.systems_heatpump import heat_pump
     from pyforge.examples.heat_pump.simulation_heatpump import compute_electrical_input
     from pyforge.examples.heat_pump.parameters_heatpump import HEATPUMP_PARAMS
-    from pyforge.examples.heat_pump.report_heatpump import HEATPUMP_PARAMS
+    from pyforge.examples.heat_pump.design import HEATPUMP_PARAMS
     
